ddeb46ccc85452779f930ebd0c4167ec25b7f02f
adrioro <artistsvillage@me.com>
2019-01-21 16:29:34 -0500

    Fourth commit

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ myApp = {}; //First we create an app object.
 myApp.selectElem = function (element) {
     return document.querySelector(element);
 }
+
+//Now, we select the elements that I need.
+
+myApp.entry = myApp.selectElem("#entry");
+myApp.output = myApp.selectElem("#output");
+
+// We create the event handler.
+
+myApp.entry.onkeyup = function () {
+    console.log("This works");
+    myApp.output.innerText = myApp.entry.value;
+}
