19dbe405e5520cb82720521c2f4853873662d2e0
adrioro <artistsvillage@me.com>
2019-01-21 15:45:58 -0500

    Second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+myApp = {}; //First we create an app object.
+
+//Getting the elements:
+
+myApp.selectElement = function (element) {
+    return document.querySelector(element);
+}
+
+var myInp = document.querySelector("#entry");
+var myOut = document.querySelector("#output");
+
+myApp.onkeyup = function () {
+    a = myInp.value = myOut.innerText;
+    (#output) innerText.write.myInp
+}
