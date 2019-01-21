5a080e1cc6f14b967da42e4fadfe0eb452fd407f
adrioro <artistsvillage@me.com>
2019-01-21 15:57:42 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 myApp = {}; //First we create an app object.
 
-//Getting the elements:
+//Then we create a method to select the elements:
 
-myApp.selectElement = function (element) {
+//Whit this method I can select any element from my document.
+myApp.selectElem = function (element) {
     return document.querySelector(element);
-}
-
-var myInp = document.querySelector("#entry");
-var myOut = document.querySelector("#output");
-
-myApp.onkeyup = function () {
-    a = myInp.value = myOut.innerText;
-    (#output) innerText.write.myInp
 }
